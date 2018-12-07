52e399ebfa77a359dfa37ed5cdf4c75b0cd222ff
Gustav Handmark <29983444+GustavHandmark@users.noreply.github.com>
2018-12-07 13:52:06 +0100

    solved mergeconflict

--- src/MyNode.java
+++ src/MyNode.java
@@ -3,6 +3,7 @@ package tree;
 /**
  * This is a node in a binary search tree. The node holds an integer value.
  * Changes 1 create conflict
+ * Changes merge conflic
  * @author Torbjorn Ekman
  * @see MyTree
  */
